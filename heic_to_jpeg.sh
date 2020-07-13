bbcdf4c944a41ff967aa97bdd9496efab39555fe
triplea-git <triplea@jb-host.net>
2020-07-13 21:28:03 +0200

    first upload

---
+++ heic_to_jpeg.sh
@@ -0,0 +1,14 @@
+#BashScript
+#title 		:heic_to_jpeg.sh
+#description 	:Convert Heic to Jpeg
+#author 	:JohnBedeir
+#website	:johnydesigns.com
+#date		:13July20
+
+
+#!/bin/bash
+for f in * .HEIC
+do
+echo "Working on file $f"
+heif-convert $f $f.jpg
+done
